3c1bc4cdb7c426718f1d0bf8f0b1d57e8060e0bc
nabechin <nabenabe0258@gmail.com>
2021-01-22 00:41:31 +0900

    add: side bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,41 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import MessageOutlinedIcon from "@material-ui/icons/MessageOutlined";
+import PermIdentityOutlinedIcon from "@material-ui/icons/PermIdentityOutlined";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  render(): JSX.Element {
+    return (
+      <div>
+        <div
+          style={{
+            width: 70,
+            flexShrink: 0,
+            height: "100vh",
+            backgroundColor: "#1a1a1a",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Divider />
+          <List>
+            <ListItem button style={{ margin: "5px 0 5px 0" }}>
+              <PermIdentityOutlinedIcon
+                fontSize="large"
+                style={{ color: "white" }}
+              />
+            </ListItem>
+            <ListItem button style={{ margin: "5px 0 5px 0" }}>
+              <MessageOutlinedIcon
+                fontSize="large"
+                style={{ color: "white" }}
+              />
+            </ListItem>
+          </List>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
