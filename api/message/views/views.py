3d205907eae5fc8cffcf4dc9e0c05b4418eb804d
nabechin <nabenabe0258@gmail.com>
2021-02-15 23:28:57 +0900

    add: jwt

--- api/message/views/views.py
+++ api/message/views/views.py
@@ -1,16 +1,21 @@
 from flask import Flask, jsonify
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt import JWT, jwt_required
 
 import message.models
 
 from message.database.database import init_db
+from message.views.auth import authenticate, identity
 
 app = Flask(__name__)
 app.config.from_object("message.config.Config")
+app.config['SECRET_KEY'] = "eafwufhafeaefaergfarf"
 init_db(app)
+
+jwt = JWT(app, authenticate, identity)
 
 
 @app.route("/", methods=["GET"])
+@jwt_required()
 def get_messages():
     messages = [
         "message1",
